2b49a3a697308e187daf5e06628a4392a2db2a37
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-09-28 14:00:48 +0700

    Enhanced website responsiveness, optimized JS, and added new features
    Enhanced website responsiveness: Improved the overall responsiveness of the website to ensure better user experience across various devices and screen sizes.
    
    Optimized JS: Refactored and optimized JavaScript code for improved performance and efficiency.
    
    Added new features: Implemented additional features to enhance functionality and user interaction.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,76 @@
+// Responsive navigation menu toggle
+
+const navBtn = document.querySelector(".nav-menu-btn")
+const navBar = document.querySelector(".nav")
+const navMenu = document.querySelector(".nav-menu")
+const navLinks = document.querySelectorAll(".nav-link")
+
+navBtn.addEventListener("click", function () {
+    navBtn.classList.toggle("close")
+    navBar.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+        navBtn.classList.remove("close")
+        navBar.classList.remove("active")
+        navMenu.classList.remove("active")
+    })
+})
+
+// Initial ScrollReveal
+
+const revealConfigurations = [
+    { selector: '.inner-title, .inner-second-title', config: { scale: 1.5, delay: 300 } },
+    { selector: '.home-info h1, .about-img, .contact-card .title', config: { delay: 300, origin: "left" } },
+    { selector: '.home-img, .description, .inner-info-link', config: { delay: 300, origin: "right" } },
+    { selector: '.skills-description, .services-description', config: { delay: 300, origin: "top" } },
+    { selector: '.media-icons a, .list-item', config: { delay: 300, origin: "top", interval: 200 } },
+    { selector: '.education', config: { delay: 300, origin: "bottom", interval: 200 } },
+    { selector: '.work-exp-title, .experience-card, .services-container, .portfolio-img-card', config: { delay: 300, origin: "bottom", interval: 200 } },
+    { selector: '.home-info h3, .home-info p, .home-info-link', config: { delay: 600, origin: "left" } },
+]
+
+function InitialScrollReveal() {
+    window.sr = ScrollReveal({
+        reset: true,
+        distance: "50px",
+        duration: 1000,
+        delay: 100
+    })
+    revealConfigurations.forEach(({ selector, config }) => {
+        sr.reveal(selector, config)
+    })
+}
+
+InitialScrollReveal()
+
+// Off ScrollReveal
+
+function disableScrollReveal() {
+    sr.clean()
+    document.documentElement.style.overflowY = "hidden"
+    document.body.style.overflowY = "hidden"
+    revealConfigurations.forEach(({ selector }) => {
+        document.querySelectorAll(selector).forEach(el => {
+            el.style.transform = ""
+            el.style.opacity = ""
+            el.style.transition = ""
+            el.style.visibility = ""
+        })
+    })
+    console.log("function off")
+}
+
+// On ScrollReveal
+function enableScrollReveal() {
+    document.documentElement.style.overflowY = ""
+    document.body.style.overflowY = ""
+    InitialScrollReveal()
+    console.log("function on")
+}
+
 // Services section - Modal
 
 const serviceModal = document.querySelectorAll(".service-modal")
@@ -6,6 +79,7 @@ const modalCloseBtn = document.querySelectorAll(".modal-close-btn")
 
 const modal = function (modalClick) {
     serviceModal[modalClick].classList.add("active")
+    disableScrollReveal()
 }
 
 learnMoreBtn.forEach((button, i) => {
@@ -19,6 +93,7 @@ modalCloseBtn.forEach(button => {
         serviceModal.forEach(modal => {
             modal.classList.remove("active")
         })
+        enableScrollReveal()
     })
 })
 
@@ -30,6 +105,7 @@ const portfolioCloseBtn = document.querySelectorAll(".portfolio-close-btn")
 
 const portfolioModal = function (modalClick) {
     portfolioModals[modalClick].classList.add("active")
+    disableScrollReveal()
 }
 
 imgCard.forEach((button, i) => {
@@ -43,24 +119,9 @@ portfolioCloseBtn.forEach(button => {
         portfolioModals.forEach(modalView => {
             modalView.classList.remove("active")
         })
+        enableScrollReveal()
     })
 })
-
-// Initialize Swiper
-
-var swiper = new Swiper(".client-swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
 
 // header effect scroll
 
@@ -112,3 +173,20 @@ scrollToTop.addEventListener("click", function () {
 //     document.body.scrollTop = 0;
 //     document.documentElement.scrollTop = 0;
 // });
+
+// Initialize Swiper
+
+var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
